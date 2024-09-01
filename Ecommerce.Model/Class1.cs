aaa888c595652647e7b5f8a9bac5664444cadf43
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2024-09-01 03:32:02 +0300

    update model class library

--- Ecommerce.Model/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ecommerce.Model
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Ecommerce.Model/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Model
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string CustomerEmail { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderProduct> OrderProducts { get; set; }
+    }
+
+    public class OrderProduct
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId {  get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
